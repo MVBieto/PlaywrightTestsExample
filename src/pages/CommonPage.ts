aadff8f619b358c7cd6a20b825bae4da1c6c0cdc
MiquelTactile <miquel@tactile.dk>
2024-11-20 14:32:31 +0100

    Test case 1 ready

--- src/pages/CommonPage.ts
+++ src/pages/CommonPage.ts
@@ -9,12 +9,22 @@ export class common {
   }
 
   //HomePage
-  get acceptPrivacyButton() { return this.page.locator('#widget-button-accept')}
-  get businessPlansButton() { return this.page.locator('a[data-ga-slug=Business]').first()}
+  get acceptPrivacyButton () { return this.page.locator('#widget-button-accept')}
+  get businessPlansButton () { return this.page.locator('a[data-ga-slug=Business]').first()}
 
   //BusinessPage
-  get businessPlansSquare() { return this.page.locator('.mx-3.w-full').all()}
-  get teamslocator () { return (this.businessPlansSquare, { hasText: 'Teams'})}
+  get tabsContentArea () { return this.page.locator('h1').getByText(`Get the plan that’s right for you`)}
+  get planCards () { return this.page.locator('.Tabs__content').locator('div.mx-3.w-full')}
+  get teamsCard () { return this.planCards.filter({ hasText: 'Teams' })}
+  get businessCard () { return this.planCards.filter({ hasText: 'Business' })}
+  get enterPriseCard () { return this.planCards.filter({ hasText: 'Enterprise' })}
+
+  get freeCard () { return this.planCards.filter({ hasText: 'Free' })}
+  get premiumCard () { return this.planCards.filter({ hasText: 'Premium' }).first()}
+  get familyCard () { return this.planCards.filter({ hasText: 'Family' })}
+
+  get priceLocators () { return this.page.locator("span.nord-text.text-base.leading-normal.text-h2.font-medium")}
+  get planPersonalSlider() { return this.page.locator('.flex.justify-center').getByText('Personal & Family')}
   
 
   async navigateToHome() {
@@ -22,10 +32,15 @@ export class common {
   }
 
   async acceptTermsNConditions() {
-    await this.clickButton(this.acceptPrivacyButton);
+    await this.acceptPrivacyButton.click()
   }
 
   async clickButton(buttonLocator: Locator) {
-    await buttonLocator.click();
+    this.waitForLocator(buttonLocator)
+    await buttonLocator.click()
+  }
+
+  async waitForLocator(buttonLocator: Locator) {
+    await buttonLocator.nth(0).waitFor({ timeout: 2000})
   }
 }
--- src/tests/homePageTest.spec.ts
+++ src/tests/homePageTest.spec.ts
@@ -1,5 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test'
 import { common } from '../pages/CommonPage';
+import 'playwright'
+import 'playwright-core'
 
 test.describe('Sanity Test - HomePage initials', () => {
   // test('Navigate to home page and validate title', async ({ page }) => {
@@ -9,20 +11,31 @@ test.describe('Sanity Test - HomePage initials', () => {
   // });
 
   test('Navigate to home page and validate business plans', async ({ page }) => {
-    const commonPage = new common(page);
-    await commonPage.navigateToHome();
+    const commonPage = new common(page)
+    await commonPage.navigateToHome()
     await commonPage.acceptTermsNConditions()
 
     //go to plans/business
     await commonPage.clickButton(commonPage.businessPlansButton)
 
-    //validate 3 main business plan panels exist
-    await expect((await commonPage.businessPlansSquare).length).toEqual(3)
-    await expect(commonPage.teamslocator).toBeDefined();
-    
-    
-    
-    await page.waitForTimeout(5000);
+    // validate 3 main business plan panels exist
+    expect(commonPage.planCards).toHaveCount(3)
+    expect(commonPage.teamsCard).toBeVisible()
+    expect(commonPage.businessCard).toBeVisible()
+    expect(commonPage.enterPriseCard).toBeVisible()
+
+    //Move to Personal Plans
+    await commonPage.clickButton(commonPage.planPersonalSlider)
+    await commonPage.waitForLocator(commonPage.freeCard)
+
+    // validate 3 main personal plan panels exist
+    expect(commonPage.planCards).toHaveCount(3)
+    expect(commonPage.freeCard).toBeVisible()
+    expect(commonPage.premiumCard).toBeVisible()
+    expect(commonPage.familyCard).toBeVisible()
+
+
+    await page.waitForTimeout(2000)
   })
 })
 
